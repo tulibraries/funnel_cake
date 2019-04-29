2f5caf7e78f7a77faf80e5253d28055791f59ebe
Steven Ng <steven@nomadicoder.com>
2019-04-29 16:55:12 -0400

    Initial traject_indexer.rb

---
+++ traject_indexer.rb
@@ -0,0 +1,57 @@
+# frozen_string_literal: true
+
+# configuration_file.rb
+# Note that "#" is a comment, cause it's just ruby
+
+settings do
+  # Where to find solr server to write to
+  provide "solr.url", "http://localhost:8983/solr"
+
+  # solr.version doesn't currently do anything, but set it
+  # anyway, in the future it will warn you if you have settings
+  # that may not work with your version.
+  provide "solr.version", "4.3.0"
+
+  # various others...
+  provide "solr_writer.commit_on_close", "true"
+
+  # DPLA MAP
+  to_field "collection", extract_xpath("//dcterms:isPartOf")
+  to_field "contributor", extract_xpath("//dcterms:contributor")
+  to_field "creator", extract_xpath("//dcterms:creator")
+  to_field "date", extract_xpath("//dcterms:date")
+  to_field "description", extract_xpath("//dcterms:description")
+  to_field "extent", extract_xpath("//dcterms:extent")
+  to_field "format", extract_xpath("//dcterms:format")
+  to_field "identifier", extract_xpath("//dcterms:identifier")
+  to_field "language", extract_xpath("//dcterms:language")
+  to_field "spatial", extract_xpath("//dcterms:spatial")
+  to_field "publisher", extract_xpath("//dcterms:publisher")
+  to_field "relation", extract_xpath("//dcterms:relation")
+  to_field "replacedBy", extract_xpath("//dcterms:isReplacedBy")
+  to_field "replaces", extract_xpath("//dcterms:replaces")
+
+  to_field "rightsHolder", extract_xpath("//dcterms:rightsholder")
+  to_field "source", extract_xpath("//dcterms:source")
+  to_field "subject", extract_xpath("//dcterms:subject")
+  to_field "genre", extract_xpath("//edm:hasType")
+  to_field "temporalCoverage", extract_xpath("//dcterms:temporal")
+  to_field "title", extract_xpath("//dcterms:title")
+  to_field "type", extract_xpath("//dcterms:type")
+
+  # edm:WebResources
+
+  to_field "fileFormat", extract_xpath("//schema:fileFormat")
+  to_field "rights", extract_xpath("//dcterms:rights")
+  to_field "rightsUri", extract_xpath("//edm:rights")
+  to_field "iiifManifest", extract_xpath("//dcterms:isReferencedBy")
+  to_field "iiifBaseUrl", extract_xpath("//svcs:hasService")
+
+  # ore:Aggregation
+
+  to_field "dataProvider", extract_xpath("edm:dataProvider")
+  to_field "url", extract_xpath("edm:isShownAt")
+  to_field "intermediateProvider", extract_xpath("dpla:intermediateProvider")
+  to_field "preview", extract_xpath("edm:preview")
+  to_field "provider", extract_xpath("edm:provider")
+end
