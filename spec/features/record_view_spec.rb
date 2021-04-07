8b7daf9520c3cd96d39b408926a80ce0f4f25fc5
David Kinzer <dtkinzer@gmail.com>
2021-04-07 16:10:25 -0400

    Add new spec.

---
+++ spec/features/record_view_spec.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe "Record View" do
+  it "displays a 'Staff View' link" do
+    VCR.use_cassette("record_view", match_requests_on: [:query]) do
+      visit solr_document_path("padig:PMA-JGJ_B015_F001_001")
+      expect(page).to have_link "Staff View"
+    end
+  end
+end
