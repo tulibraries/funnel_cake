6b77d7ddde6c1dd6d6fcea5b7f28807b1b6d4f6d
Steven Ng <steven.ng@temple.edu>
2024-07-24 15:17:09 -0600

    Adjust navbar form element sizes

--- app/components/blacklight/search_button_component.rb
+++ app/components/blacklight/search_button_component.rb
@@ -8,7 +8,7 @@ module Blacklight
     end
 
     def call
-      tag.button(class: 'btn btn-primary search-btn', type: 'submit', id: @id) do
+      tag.button(class: "btn btn-primary search-btn", type: "submit", id: @id) do
         tag.span(@text, class: "me-sm-1 submit-search-text") +
           render(Blacklight::Icons::SearchComponent.new)
       end
