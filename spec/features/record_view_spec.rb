ad433c8064b05c548103bde6a5a83175fe30babe
Chris Doyle <cdoyle@temple.edu>
2024-10-23 08:56:07 -0400

    fix test

--- spec/features/record_view_spec.rb
+++ spec/features/record_view_spec.rb
@@ -6,7 +6,8 @@ RSpec.describe "Record View" do
   it "displays a 'Staff View' link" do
     VCR.use_cassette("record_view", match_requests_on: [:query]) do
       visit solr_document_path("padig:PMA-JGJ_B015_F001_001")
-      expect(page).to have_link "Staff View"
+      expect(page).to have_text "Staff View"
+      expect(page).to have_link(href: "/catalog/padig:PMA-JGJ_B015_F001_001.xml")
     end
   end
 end
